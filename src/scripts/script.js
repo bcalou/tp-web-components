041096c1aea5f734742f0980a83b9deda6e816d6
bcalou <bastien.calou@gmail.com>
2023-11-12 19:31:10 +0100

    handle app install

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,7 +2,34 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js");
 }
 
+let installPrompt;
+const pwaInstallButton = document.getElementById("pwaInstall");
+
+// When the browser detects that the app can be installed
 window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
-  document.getElementById("pwaInstall").style.display = "initial";
+  installPrompt = event;
+  pwaInstallButton.style.display = "initial";
 });
+
+// On click on the install button
+pwaInstallButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
+});
+
+// Once the app is installed
+window.addEventListener("appinstalled", () => {
+  disableInAppInstallPrompt();
+});
+
+// Disable the installation process
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  pwaInstallButton.style.display = "none";
+}
