cd99fae1b9109b64bcc3517c4bea27aa3254322b
bcalou <bastien.calou@gmail.com>
2023-11-15 17:47:41 +0100

    test

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -40,3 +40,32 @@ function disableInAppInstallPrompt() {
   installPrompt = null;
   pwaInstallButton.style.display = "none";
 }
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCD2F_QbXE5c2BAbcJA8cWgK77dyOh6vEM",
+  authDomain: "bcalou-pwa.firebaseapp.com",
+  projectId: "bcalou-pwa",
+  storageBucket: "bcalou-pwa.appspot.com",
+  messagingSenderId: "120929146497",
+  appId: "1:120929146497:web:9507edaf73e22e37469f80"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.getToken({ vapidKey: 'BHQcYkFFS-giLWiENBzd9GS2mVs_E0tcDPYfJWuOXpXVzBmD5jz9wCwiCyEgbsGiOWtWjlWhYFiTtfDvjx1LSgQ' })
+.then(currentToken => {
+  if (currentToken) {
+    console.log(currentToken);
+    return currentToken;
+  } else {
+    console.log('No registration token available. Request permission to generate one.');
+  }
+}).catch(err => {
+  console.log('An error occurred while retrieving token. ', err);
+});
